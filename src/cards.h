5c8b8700ea0e3fa06c29b43b464fd544e753bbc4
zelfeim <zelfeim@mailbox.org>
2022-07-05 19:04:37 +0200

    Changed card value initalization

--- src/cards.h
+++ src/cards.h
@@ -2,18 +2,29 @@
 #define CARDS_H_
 
 enum class Color { Diamond, Club, Heart, Spade };
-enum class Face  { Ace, Two, Three, Four, Five,
-                   Six, Seven, Eight, Nine, Ten,
-                   Jack, Queen, King };
+enum class Face  {
+    Ace = 11,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Jack = 10,
+    Queen = 10,
+    King = 10
+};
 
 class Card {
     const Color color_;
     const Face face_;
-    int value_;
+    const int value_;
 
 public:
     Card(const Color, const Face);
-    int ret_card_value();
 
     int value() const;
 };
