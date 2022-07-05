1d3a9245545b708bb40926a5033f1fa008500d2b
zelfeim <zelfeim@mailbox.org>
2022-07-06 01:55:29 +0200

    Now more than one deck of cards is created

--- src/deck.cpp
+++ src/deck.cpp
@@ -1,9 +1,9 @@
 #include "deck.h"
 
-#include <iostream>
-
 // initializes the deck and all its cards
-Deck::Deck() : cards_() {
+Deck::Deck(const int& number_of_decks) : cards_() {
+    // creating arrays so that we can create
+    // entire decks having every possible suit and color combination
     std::array<Color, 4> colors {Color::Club,
                                  Color::Diamond,
                                  Color::Heart,
@@ -18,9 +18,13 @@ Deck::Deck() : cards_() {
                                 Face::King};
 
 
-    for(auto& color : colors) {
-        for(auto& face : faces) {
-            cards_.emplace_back(Card(color, face));
+    // loops that create 52 different cards x times,
+    // x meaning number of used decks
+    for(int i = 0; i < number_of_decks; ++i) {
+        for(auto& color : colors) {
+            for(auto& face : faces) {
+                cards_.emplace_back(Card(color, face));
+            }
         }
     }
 }
--- src/deck.h
+++ src/deck.h
@@ -3,13 +3,17 @@
 
 #include <vector>
 #include <array>
+#include <random>
+#include <algorithm>
 
 #include "cards.h"
 
 class Deck {
     std::vector<Card> cards_;
 public:
-    Deck();
+    Deck(const int&);
+
+    void shuffle_deck();
 };
 
 #endif // DECK_H_
