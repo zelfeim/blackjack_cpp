63e9b56c5d9534f318ee4ee64804657578b9ee2b
zelfeim <zelfeim@mailbox.org>
2022-07-08 02:11:17 +0200

    Changed constructor member initalization, added card name

--- src/cards.cpp
+++ src/cards.cpp
@@ -2,8 +2,129 @@
 
 Card::Card(const Color color, const Face face) : color_(color),
                                                  face_(face),
-                                                 value_(static_cast<int>(face)) {}
+                                                 value_(init_value(face)),
+                                                 name_(init_name(face, color)) {}
 
+// returns the correct value of face for constructors member initalization
+int init_value(const Face& face) {
+    switch(face) {
+        case(Face::Ace):
+            return 11;
+            break;
+        case(Face::Two):
+            return 2;
+            break;
+        case(Face::Three):
+            return 3;
+            break;
+        case(Face::Four):
+            return 4;
+            break;
+        case(Face::Five):
+            return 5;
+            break;
+        case(Face::Six):
+            return 6;
+            break;
+        case(Face::Seven):
+            return 7;
+            break;
+        case(Face::Eight):
+            return 8;
+            break;
+        case(Face::Nine):
+            return 9;
+            break;
+        case(Face::Ten):
+            return 10;
+            break;
+        case(Face::Jack):
+            return 10;
+            break;
+        case(Face::Queen):
+            return 10;
+            break;
+        case(Face::King):
+            return 10;
+            break;
+    }
+}
+
+// returns string which is the displayed name of such card,
+// depending on its face and color
+std::string init_name(const Face& face, const Color& color) {
+    std::string temp {""};
+
+    // appends the face name
+    switch(face) {
+        case(Face::Ace):
+            temp += "Ace";
+            break;
+        case(Face::Two):
+            temp += "Two";
+            break;
+        case(Face::Three):
+            temp += "Three";
+            break;
+        case(Face::Four):
+            temp += "Four";
+            break;
+        case(Face::Five):
+            temp += "Five";
+            break;
+        case(Face::Six):
+            temp += "Six";
+            break;
+        case(Face::Seven):
+            temp += "Seven";
+            break;
+        case(Face::Eight):
+            temp += "Eight";
+            break;
+        case(Face::Nine):
+            temp += "Nine";
+            break;
+        case(Face::Ten):
+            temp += "Ten";
+            break;
+        case(Face::Jack):
+            temp += "Jack";
+            break;
+        case(Face::Queen):
+            temp += "Queen";
+            break;
+        case(Face::King):
+            temp += "King";
+            break;
+    }
+
+    temp += " of ";
+
+    // appends color name
+    switch(color) {
+        case(Color::Diamond):
+            temp += "Diamonds";
+            break;
+        case(Color::Club):
+            temp += "Clubs";
+            break;
+        case(Color::Heart):
+            temp += "Hearts";
+            break;
+        case(Color::Spade):
+            temp += "Spades";
+            break;
+    }
+
+    return temp;
+}
+
+// returning value
 int Card::value() const {
     return value_;
+}
+
+// returning name
+std::string Card::name() const {
+    return name_;
 }
--- src/cards.h
+++ src/cards.h
@@ -1,32 +1,26 @@
 #ifndef CARDS_H_
 #define CARDS_H_
 
+#include <string>
+
 enum class Color { Diamond, Club, Heart, Spade };
-enum class Face  {
-    Ace = 11,
-    Two = 2,
-    Three = 3,
-    Four = 4,
-    Five = 5,
-    Six = 6,
-    Seven = 7,
-    Eight = 8,
-    Nine = 9,
-    Ten = 10,
-    Jack = 10,
-    Queen = 10,
-    King = 10
-};
+enum class Face  { Ace, Two, Three, Four, Five,
+    Six, Seven, Eight, Nine, Ten, Jack, Queen, King };
 
 class Card {
     const Color color_;
     const Face face_;
     const int value_;
+    const std::string name_;
 
 public:
-    Card(const Color, const Face);
+    Card(const Color&, const Face&);
+
+    int init_value(const Face&);
+    std::string init_name(const Face&, const Color&);
 
     int value() const;
+    std::string name() const;
 };
 
 #endif // CARDS_H_
